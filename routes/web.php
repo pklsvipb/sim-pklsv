6007cfa89c7f8233c583d836a6e2b41ced9534ba
pklsvipb <99707935+pklsvipb@users.noreply.github.com>
2022-03-17 14:57:07 +0700

    tes cicd

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
-    return 1;
+    return 123;
 });
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'login'])->name('login');
