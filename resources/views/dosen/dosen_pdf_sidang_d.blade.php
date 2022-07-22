285a7c6dd837232840ee63253c2134c765622ce9
abdullatifw <abdullatifw10@gmail.com>
2022-07-22 13:01:02 +0630

    tes

--- resources/views/dosen/dosen_pdf_sidang_d.blade.php
+++ resources/views/dosen/dosen_pdf_sidang_d.blade.php
@@ -185,7 +185,7 @@
         <td><p style="margin-bottom: -10em; font-size: 11pt;">Kelulusan</p></td>
         <td><p style="margin-bottom: -10em; font-size: 11pt;">:</p></td>
         <td colspan="2"><p style="margin-bottom: -10em; font-size: 11pt;">
-          @if ((($bap->nilai1 +  $bap->nilai2 + $bap->nilai3) / 3) => 65)
+          @if ((($bap->nilai1 +  $bap->nilai2 + $bap->nilai3) / 3) > 64)
             Lulus / <s>Tidak Lulus</s> *)
           @else
             <s>Lulus</s> / Tidak Lulus *)
